4458702b967edb4ba381c1a9e8a91861f8dad2f4
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-25 21:05:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,9 +13,6 @@ def reduce(source_array, starting_point = 0)
   
      ret_value = starting_point
   
-    
- end
-
   i = 0 # set up a i for the enumeration 
   while i < source_array.length do # a loop for each element in array
     ret_value = yield(ret_value, source_array[i])
